939c7ba7c36b8795ce2af2a8b6a9a84a5a916a25
thomps0189 <angelathompson2011@gmail.com>
2021-11-08 19:18:14 -0600

    can add list item from text box

---
+++ assets/index.js
@@ -0,0 +1,45 @@
+var currentDay = document.getElementById("currentDay");
+var taskContent = document.getElementById("task-content");
+var listUlEl = document.getElementById("list-todo");
+var mySchedule = []
+
+// var tasks = localStorage.getItem("mySchedule")
+// ? JSON.parse(localStorage.getItem("mySchedule"))
+// : localStorage.setItem("mySchedule", JSON.stringify(mySchedule))
+
+// var showCurrentDay = function() {
+//     console.log
+// }
+
+// console.log(listUlEl)
+
+// $("#task-content").on("click", function() {
+//     console.log("Hello")
+//     var text = $(this)
+//     .text()
+//     .trim();
+//     var textInput = $("<textarea>")
+//     .addClass("form-control")
+//     .val(text);
+//     $(this).replaceWith(textInput)
+// })
+
+// $("#task-content").on("click", function() {
+//     if(tasks.length > 0) {
+//         tasks.forEach(task => {
+//             let listEl = document.createElement("li")
+//             listEl.innerText = task
+//             listUlEl.appendChild(listEl)
+//         })
+//     }
+// })
+
+function addLi() {
+    var textVal = document.getElementById("textVal").value,
+    listNode = document.getElementById("list-todo"),
+    liNode = document.createElement("LI"),
+    textNode = document.createTextNode(textVal);
+
+    liNode.appendChild(textNode);
+    listNode.appendChild(liNode);
+}
