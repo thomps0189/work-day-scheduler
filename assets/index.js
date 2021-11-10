825770b44f0cf1feb75764151f79e19cd0fbdcfd
thomps0189 <angelathompson2011@gmail.com>
2021-11-10 13:01:42 -0600

    local storage

--- assets/index.js
+++ assets/index.js
@@ -1,60 +1,27 @@
 var date = moment().format("LLLL");
 document.getElementById("currentDay").innerHTML = date;
+var button = document.querySelector(".btn");
+var newTask = document.getElementById("task");
 
 var nine = document.getElementById("9oclock")
 var taskText = document.getElementById("tasktext");
 var myTasks = [];
 var tasks = localStorage.getItem("myTasks") ? JSON.parse(localStorage.getItem("myTasks")) : localStorage.setItem("myTasks", JSON.stringify(myTasks))
 
-// var taskContent = document.getElementById("task-content");
-// var listUlEl = document.getElementById("list-todo");
-// var mySchedule = []
+function handleSubmitEvent(event) {
+    event.preventDefault()
+    var inputValue = tasks.value 
 
-// $("#task-content").on("click", "input", function() {
-//     var text = $(this).text();
-//     console.log(text)
-// });
-
-// $("#submit-btn").click(function() {
-//     console.log("submit button clicked");
-// });
-
-// $("#clear-btn").click(function() {
-//     console.log("this is the clear button")
-// })
+    if (!inputValue) {
+        console.log("enter a tasks")
+        return
+    }
+    tasks.push(inputValue)
+    localStorage.setItem("myTasks", JSON.stringify(tasks))
 
 
+}
 
 
+button.addEventListener("submit", handleSubmitEvent)
 
-
-// var tasks = localStorage.getItem("mySchedule")
-// ? JSON.parse(localStorage.getItem("mySchedule"))
-// : localStorage.setItem("mySchedule", JSON.stringify(mySchedule))
-
-// var showCurrentDay = function() {
-//     console.log
-// }
-
-// console.log(listUlEl)
-
-// $("#task-content").on("click", function() {
-//     console.log("Hello")
-//     var text = $(this)
-//     .text()
-//     .trim();
-//     var textInput = $("<textarea>")
-//     .addClass("form-control")
-//     .val(text);
-//     $(this).replaceWith(textInput)
-// })
-
-// $("#task-content").on("click", function() {
-//     if(tasks.length > 0) {
-//         tasks.forEach(task => {
-//             let listEl = document.createElement("li")
-//             listEl.innerText = task
-//             listUlEl.appendChild(listEl)
-//         })
-//     }
-// })
