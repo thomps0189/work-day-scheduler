083157c17596959d13c72570e5a6ede6d71b4934
thomps0189 <angelathompson2011@gmail.com>
2021-11-11 19:08:12 -0600

    updated

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,7 @@ document.getElementById("currentDay").innerHTML = date;
 
 // fill in and save timeslot items
 var myTasks = [];
+// call function to get data
 
 var addTask = (event) =>{
     event.preventDefault();
@@ -21,8 +22,8 @@ var addTask = (event) =>{
 
     // save to local storage
     localStorage.setItem("MyTaskList", JSON.stringify(myTasks));
-    var x = localStorage.getItem("MyTasksList");
-    document.getElementById("tasktext").innerHTML = x
+    // var x = localStorage.getItem("MyTasksList");
+    // document.getElementById("tasktext").innerHTML = x
     
     // for (let i = 0; i < localStorage.length; i++) {
     //     var key = localStorage.key(i);
@@ -30,8 +31,67 @@ var addTask = (event) =>{
 
     //     myTasks.innerHTML += `${key}: ${value}`
     // }
+
+    if (moment().isAfter(date)) {
+        $(task).addClass("list-group-item-danger");
+    } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+        $(task).addClass("list-group-item-warning");
+    }
          
 }
+
+var timeBlockHour = function() {
+    // what is the present time in hours
+    var presentTime = moment().hour();
+    console.log(presentTime);
+
+    // what is the block of time in hours
+    // var hourBlockNumber = document.getElementsByClassName("hour9");
+
+    // Do I need to differintiate between am and pm
+    
+    var hourBlock = parseInt("6block") + 12;
+    console.log(hourBlock)
+
+    // $(".hour").each(function() {
+    //     var hourBlock = parseInt($(this).attr(".hour").split(".hour")[1]);
+    // })
+
+    if (hourBlock < presentTime) {
+        $("textarea").addClass("past")
+    }
+
+    if (hourBlock === presentTime) {
+        $("textarea").addClass("present")
+    }
+
+    if (hourBlock > presentTime) {
+        $("textarea").addClass("future");
+        $("textarea").removeClass("past");
+    }
+
+    // var hourBlock6 = parseInt("9block")
+    // console.log(hourBlock)
+
+    // // $(".hour").each(function() {
+    // //     var hourBlock = parseInt($(this).attr(".hour").split(".hour")[1]);
+    // // })
+
+    // if (hourBlock6 < presentTime) {
+    //     $("textarea").addClass("past")
+    // }
+
+    // if (hourBlock6 === presentTime) {
+    //     $("textarea").addClass("present")
+    // }
+
+    // if (hourBlock6 > presentTime) {
+    //     $("textarea").addClass("future");
+    //     $("textarea").removeClass("past");
+    // }
+}
+
+timeBlockHour()
 
 document.getElementById("btn9").addEventListener("click", addTask)
 
